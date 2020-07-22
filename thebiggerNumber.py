803dae645f89be9486fc31c3c2caf557c5ec8a76
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 02:16:40 +0100

    Create thebiggerNumber.py

---
+++ thebiggerNumber.py
@@ -0,0 +1,9 @@
+num1 = int(input("What is the first number? ---> "))
+num2 = int(input("What is the second number? ---> "))
+
+if num1 > num2:
+    print(num1 , " is bigger!")
+elif num2 > num1:
+    print(num2 , " is bigger!")
+else:
+    print("They are equal!!!")
