96c85cf010b1ea29a8f024a05767bbb9f2220120
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 12:25:02 +0100

    Create triviaQuiz.py

---
+++ triviaQuiz.py
@@ -0,0 +1,71 @@
+points = 0
+
+question1 = input("Who is the smallest NBA player? ----> ")
+
+if question1 == "Thomas" or question1 == "thomas":
+
+print("correct :)")
+
+points = points + 1
+
+else:
+
+print("wrong")
+
+question2 = input("What Polish basketball player play in the NBA? ----> ")
+
+if question2 == "Gortat" or question2 == "gortat":
+
+print("correct :)")
+
+points = points + 1
+
+else:
+
+print("wrong")
+
+question3 = input("What kind of ball do they use in the NBA? ----> ")
+
+if question3 == "Spalding" or question3 == "spalding":
+
+print("correct :)")
+
+points = points + 1
+
+else:
+
+print("wrong")
+
+question4 = input("Most famous player with the number 23? ----> ")
+
+if question4 == "Jordan" or question4 == "jordan":
+
+print("correct :)")
+
+points = points + 1
+
+else:
+
+print("wrong")
+
+question5 = input("Where does Lebron play? ----> ")
+
+if question5 == "Cavs" or question5 == "cavs":
+
+print("correct :)")
+
+points = points + 1
+
+else:
+
+print("wrong")
+
+print(points)
+
+if points > 3:
+
+print("Nice you know the NBA!!! :)")
+
+else:
+
+print("Try better next time!!! :(")
