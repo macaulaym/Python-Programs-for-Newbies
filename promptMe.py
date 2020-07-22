a6cd95f0ef19d1eb2534e34dad43eb8d38c7b6d7
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 02:07:19 +0100

    Create promptMe.py

---
+++ promptMe.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+
+age = int(input("Please enter your age: "))
+
+cartoon = (input("Please enter your favourite cartoon: "))
+
+print("Your name is ", name, "and you are ", age, "years old. Your favourite cartoon is ", cartoon)
