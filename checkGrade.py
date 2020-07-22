01d5df173a19b88c105b9626aed5961b80d0d6fc
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 12:22:36 +0100

    Create checkGrade.py

---
+++ checkGrade.py
@@ -0,0 +1,29 @@
+mark = float(input("What is your grade? ---> "))
+
+if mark > 89:
+
+print("You got an A")
+
+elif mark > 74:
+
+print("You got an B")
+
+elif mark > 59:
+
+print("You got an C")
+
+elif mark > 49:
+
+print("You got an D")
+
+elif mark > 34:
+
+print("You got an E")
+
+elif mark < 35:
+
+print("You failed")
+
+else:
+
+print("Sorry we could not check your score!")
