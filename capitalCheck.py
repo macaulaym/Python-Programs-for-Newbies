038305bab32daf23f4b0a0789a0ab312cc65ced7
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 12:26:38 +0100

    Create capitalCheck.py

---
+++ capitalCheck.py
@@ -0,0 +1,13 @@
+capital = input("What is the capital of Brazil? --> ")
+
+while capital != "Brasilia" or capital != "brasilia":
+
+   print("Wrong!")
+
+  capital = input("Try again ---> ")
+
+if capital == "Brasilia" or capital == "brasilia":
+
+  break
+
+  print("Well Done!")
