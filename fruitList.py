5dbc3136af0db4464ce1ec3198df130c2fa5eb53
macaulaym <macaulaym@users.noreply.github.com>
2020-07-22 13:52:30 +0100

    Create fruitList.py

---
+++ fruitList.py
@@ -0,0 +1,14 @@
+def GettingFruity():
+
+fruitList = ['apple','orange','strawberry','pear']
+
+userFruit = input("Please enter your favourite fruit, in singular and lowercase: ")
+
+if userFruit in fruitList:
+
+   print("I like that fruit too!")
+
+else:
+
+  print("Sorry, I don't that fruit much.")
+
