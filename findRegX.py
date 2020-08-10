3fdeaefd0883bab0aa922b7325293f1666c94288
Michael Macaulay <macaulaym@users.noreply.github.com>
2020-08-10 21:16:47 +0100

    Add files via upload

---
+++ findRegX.py
@@ -0,0 +1,8 @@
+import re
+
+if re.search("latest", "Please provide update your supervisor with the latest information"):
+
+    print("The system has searched and found the word: latest in the string provided")
+
+else:
+    print("No instances of the word latest found in the string")
