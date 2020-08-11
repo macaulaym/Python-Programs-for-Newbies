4c29d2a65c4de0d51651927874f44f11b59549b4
Michael Macaulay <macaulaym@users.noreply.github.com>
2020-08-11 09:05:48 +0100

    Add files via upload

---
+++ findPattern.py
@@ -0,0 +1,9 @@
+import re
+
+str = "Sat, hat, mat, pat"
+
+#find words that start with s/h/m/p and end with "at" in the string provided.
+allstr = re.findall("[shmp]at", str)
+
+for i in allstr:
+    print(i)
